d67e5b100bbfc111ac139287ea3dfd1fba6f07de
UJJWALRANJAN <UJJWALRANJAN88@GMAIL.COM>
2018-07-05 13:46:54 +0530

    all file uploaded

---
+++ includes/navbar.php
@@ -0,0 +1,12 @@
+<div class="navbar">
+	<div class="logo_div">
+		<a href="index.php"><h1>Grutebox</h1></a>
+	</div>
+	<ul>
+	  <li><a class="active" href="index.php">Home</a></li>
+	  <li><a href="#news">Living Walls</a></li>
+	  <li><a href="#rooftop">Rooftop Gardening</a></li>
+		<li><a href="#Balcony">Balcony Gardening</a></li>
+	  <li><a href="#about">About</a></li>
+	</ul>
+</div>
